2d27b9457b36d6c65e46727ec223ab618fb6061a
mikekucek <mikekucek@users.noreply.github.com>
2018-05-27 16:43:42 -0500

    initial transitions

--- src/components/Show/SeasonDetail.jsx
+++ src/components/Show/SeasonDetail.jsx
@@ -1,27 +1,34 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
+// https://github.com/reactjs/react-transition-group/tree/v1-stable
+// implement this at some point
+// for unmount transitions
+
 class SeasonDetail extends Component {
   render() {
     const { selectedSeason } = this.props
     return (
-      <table>
-        <tbody>
-          {selectedSeason.map((ep) => {
-            return (
-              <tr key={ep.id}>
-                <td>
-                  <img style={{ maxWidth: '180px' }} src={ep.image.medium} alt={`${ep.id} Placeholder`} />
-                </td>
-                <td>{ep.number} {ep.name}</td>
-                <td>{ep.airDate}</td>
-                <td>{ep.summary ? ep.summary.replace(/<[^>]+>/g, '') : 'Spoilers, sweetie!'}</td>
-              </tr>
-            )
-          })
-          }
-        </tbody>
-      </table>
+      <div className="season-detail-container">
+        <table>
+          <tbody>
+            {selectedSeason.map((ep) => {
+              return (
+                <tr key={ep.id}>
+                  <td>
+                    <div className="episode-detail-header">
+                      <img style={{ maxWidth: '180px' }} src={ep.image.medium} alt={`${ep.id} Placeholder`} />
+                      <span>{ep.number} {ep.name}</span>
+                    </div>
+                  </td>
+                  <td className="episode-detail-summary">{ep.summary ? ep.summary.replace(/<[^>]+>/g, '') : 'Spoilers, sweetie!'}</td>
+                </tr>
+              )
+            })
+            }
+          </tbody>
+        </table>
+      </div>
     )
   }
 }
