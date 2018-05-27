a1c6418bfc69d4060a0131f75708a9dcde6245c1
mikekucek <mikekucek@users.noreply.github.com>
2018-05-26 21:29:07 -0500

    async api function

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 
-export function getShowFromAPI(show) {
+export function _getShowFromAPI(show) {
   return Promise.all([
     fetch(`http://api.tvmaze.com/shows/${show}`),
     fetch(`http://api.tvmaze.com/shows/${show}/episodes`)
@@ -22,4 +22,24 @@ export function getShowFromAPI(show) {
           watchedCount: Math.floor(Math.random() * eData.length)
         }
       }))
+}
+
+export async function getShowFromAPI(show) {
+  const [s, e] = await Promise.all([fetch(`http://api.tvmaze.com/shows/${show}`), fetch(`http://api.tvmaze.com/shows/${show}/episodes`)])
+  const [sData, eData] = await Promise.all([s.json(), e.json()])
+  console.log('fetched', sData.name)
+  const seasons = eData[eData.length - 1].season
+  const episodes = Array.from({ length: seasons }, (v, i) => []);
+  eData.map(ep => {
+    episodes[ep.season - 1].push(ep)
+  })
+  return {
+    id: sData.id,
+    name: sData.name,
+    status: sData.status,
+    image: sData.image.medium,
+    episodeCount: eData.length,
+    episodes,
+    watchedCount: Math.floor(Math.random() * eData.length)
+  }
 }
